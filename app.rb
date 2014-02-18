39f4f13d952a148cb3ae253a76d1225ed461c71b
Tom Close <tom.close@cantab.net>
2014-02-18 13:52:37 +0000

    Add slightly drier version and helper functions.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'csv'
+# require './helper_functions'
 
 get '/' do
   erb :index
@@ -37,3 +38,29 @@ post '/' do
 
   erb :tickets
 end
+
+# post '/' do
+#   @event   = params[:event]
+#   @swimmer = params[:swimmer]
+#   @cyclist = params[:cyclist]
+#   @runner  = params[:runner]
+
+#   @event_name = event_name(@event)
+#   @event_date = event_date(@event)
+
+#   @swimmer_first_name   = first_name(@swimmer[:name])
+#   @swimmer_last_name    = last_name(@swimmer[:name])
+#   @swimmer_dob          = @swimmer[:dob]
+
+#   @cyclist_first_name   = first_name(@cyclist[:name])
+#   @cyclist_last_name    = last_name(@cyclist[:name])
+#   @cyclist_dob          = @cyclist[:dob]
+
+#   @runner_first_name    = first_name(@runner[:name])
+#   @runner_last_name     = last_name(@runner[:name])
+#   @runner_dob           = @runner[:dob]
+
+#   erb :tickets
+# end
+
+
