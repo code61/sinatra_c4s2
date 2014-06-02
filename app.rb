f4dec1edae9abcf70b048e4fa9848767482513cb
George Knee <georgeknee@googlemail.com>
2014-06-02 13:30:43 +0100

    refactored using Person class

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require './helper_functions'
-# require './person'
+ require './person'
 # require './event'
 
 get '/' do
@@ -8,38 +8,38 @@ get '/' do
 end
 
 
-post '/' do
-  @event   = params[:event]
-  @swimmer = params[:swimmer]
-  @cyclist = params[:cyclist]
-  @runner  = params[:runner]
+#post '/' do
+  #@event   = params[:event]
+  #@swimmer = params[:swimmer]
+  #@cyclist = params[:cyclist]
+  #@runner  = params[:runner]
 
-  @event_name = event_name(@event)
-  @event_date = event_date(@event)
+  #@event_name = event_name(@event)
+  #@event_date = event_date(@event)
 
-  @swimmer_first_name   = first_name(@swimmer[:name])
-  @swimmer_last_name    = last_name(@swimmer[:name])
-  @swimmer_dob          = @swimmer[:dob]
+  #@swimmer_first_name   = first_name(@swimmer[:name])
+  #@swimmer_last_name    = last_name(@swimmer[:name])
+  #@swimmer_dob          = @swimmer[:dob]
 
-  @cyclist_first_name   = first_name(@cyclist[:name])
-  @cyclist_last_name    = last_name(@cyclist[:name])
-  @cyclist_dob          = @cyclist[:dob]
+  #@cyclist_first_name   = first_name(@cyclist[:name])
+  #@cyclist_last_name    = last_name(@cyclist[:name])
+  #@cyclist_dob          = @cyclist[:dob]
 
-  @runner_first_name    = first_name(@runner[:name])
-  @runner_last_name     = last_name(@runner[:name])
-  @runner_dob           = @runner[:dob]
+  #@runner_first_name    = first_name(@runner[:name])
+  #@runner_last_name     = last_name(@runner[:name])
+  #@runner_dob           = @runner[:dob]
 
-  erb :tickets
-end
+  #erb :tickets
+#end
 
-# post '/' do
-#   @event   = Event.new(params[:event])
-#   @swimmer = Person.new(params[:swimmer])
-#   @cyclist = Person.new(params[:cyclist])
-#   @runner  = Person.new(params[:runner])
+ post '/' do
+  # @event   = Event.new(params[:event])
+   @swimmer = Person.new(params[:swimmer])
+   @cyclist = Person.new(params[:cyclist])
+   @runner  = Person.new(params[:runner])
 
-#   erb :tickets
-# end
+   erb :tickets
+ end
 
 
 
--- person.rb
+++ person.rb
@@ -1,4 +1,54 @@
+# Describes a person
+#
+# Examples:
+#
+#   p = Person.new(:name => "Bart Simpson", :dob => "15/1/1990")
+#
+#   p.name       #=> "Bart Simpson"
+#   p.first_name #=> "Bart"
+#   p.last_name  #=> "Simpson"
+#   p.dob        #=> "15/1/1990"
+#
 
 class Person
 
+	def initialize(params)
+		@first_name = params[:name].split.first
+		if params[:name] == ""
+			@last_name = ""
+		else
+			@last_name = params[:name].split[1..-1].join(' ')
+		end
+	
+		@dob = params[:dob]
+	end
+
+	def name
+		[@first_name,@last_name].join(" ")
+	end
+
+	def dob
+		@dob
+	end
+
+	def first_name
+		@first_name 
+	end
+
+	def last_name
+		@last_name 
+	end
+
+	def dob=(new_dob)
+		@dob = new_dob
+	end
+
+	def first_name=(new_first_name)
+		@first_name = new_first_name
+	end
+
+	def last_name=(new_last_name)
+		@last_name = new_last_name
+	end
 end
+
